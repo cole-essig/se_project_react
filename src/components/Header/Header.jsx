ddd06613a246f84285f88d67d0cef87246df1fb3
Cole Essig <hoopwalker@yahoo.com>
2024-09-28 16:16:07 -0700

    Added links to main page to navigate routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.css'
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg'
 import avatar from '../../assets/avatar.svg'
 import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
@@ -7,14 +8,18 @@ function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
     return (
       <header className='header'>
-        <img className='header__logo' alt='Logo' src={logo} />
+        <Link to="/">
+          <img className='header__logo' alt='Logo' src={logo} />
+        </Link>
         <p className='header__date-time'>{currentDate}, {weatherData.city}</p>
         <ToggleSwitch />
         <button type='button' onClick={handleAddClick} className='header__add-clothes-btn'>+ Add clothes</button>
-        <div className='header__user-container'>
-          <p className='header__username'>Terrence Tegegne</p>
-          <img src={avatar} alt='User Avatar photo' className='Header__avatar' />
-        </div>
+        <Link to='/profile' className='header__link'>
+          <div className='header__user-container'>
+            <p className='header__username'>Terrence Tegegne</p>
+            <img src={avatar} alt='User Avatar photo' className='Header__avatar' />
+          </div>
+        </Link>
       </header>
     )
 }
